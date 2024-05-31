29eceb175255376ba6ddaf91ca86ec8328e9ecd3
Arnab Adhikary <aarnab11@gmail.com>
2024-06-01 00:55:16 +0530

    added private routing

--- src/components/AllRoutes.jsx
+++ src/components/AllRoutes.jsx
@@ -1,19 +1,49 @@
-import { Routes, Route} from "react-router-dom"
+import { Routes, Route, Navigate} from "react-router-dom"
 import Home from "../pages/Home"
 import About from "../pages/About"
 import Contact from "../pages/Contact"
 import Login from "../pages/Login"
 import Ticket from "../pages/Ticket"
+import { useContext } from "react"
+import { AuthContext } from "../Context/AuthContextProvider"
+
+function PrivateRoute({children}){
+    const { authDetails } = useContext(AuthContext)
+    
+    if(!authDetails?.isLoggedIn){
+        return <Navigate to="/login" />
+    }
+
+    return children
+}
 
 export default function AllRoutes(){
     return(
         <>
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/Contact" element={<Contact />} />
-            <Route path="/ticket" element={<Ticket />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={
+                <PrivateRoute>
+                <Home />
+                </PrivateRoute>
+            } />
+            <Route path="/about" element={
+                <PrivateRoute>
+                    <About />                   
+                </PrivateRoute>
+            } />
+            <Route path="/Contact" element={
+                <PrivateRoute>
+                    <Contact />
+                </PrivateRoute>
+            } />
+            <Route path="/ticket" element={
+            <PrivateRoute>
+                <Ticket />
+            </PrivateRoute>
+            } />
+            <Route path="/login" element={
+                <Login />
+            } />
         </Routes>
         </>
     )
